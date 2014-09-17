63c1a09d8f3eed4203044c62d65d4aab740be55b
ayudina <anna.yudina16@gmail.com>
2014-09-17 14:26:52 -0600

    Update Helloworld.java

--- eclipse/AnnaYudinaLab4/Lab4Test/src/helloworld/Helloworld.java
+++ eclipse/AnnaYudinaLab4/Lab4Test/src/helloworld/Helloworld.java
@@ -5,6 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World!");// Display the string
 		System.out.println("How are you?"); 
+		// testing eclipse and GitHub
 
 	}
 
